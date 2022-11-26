9fb238a3f1ef800db2f5ba92885f9006e5e145fb
triyolanda <triyolanda181198@gmail.com>
2022-11-26 14:01:46 +0700

    ppp

---
+++ app/Http/Controllers/PenjualanDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\penjualan_detail;
+use Illuminate\Http\Request;
+
+class PenjualanDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\penjualan_detail  $penjualan_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(penjualan_detail $penjualan_detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\penjualan_detail  $penjualan_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(penjualan_detail $penjualan_detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\penjualan_detail  $penjualan_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, penjualan_detail $penjualan_detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\penjualan_detail  $penjualan_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(penjualan_detail $penjualan_detail)
+    {
+        //
+    }
+}
